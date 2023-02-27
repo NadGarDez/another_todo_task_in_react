15ed3d7bf2668e061d8d5ff1918beabd518d4e41
Nad GarDez <iranad@hotmail.com>
2023-02-27 13:10:42 -0400

    feat: added state to search component, class 9

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/toDoSearch.css"
 
 export const TodoSearch = ()=> {
+
+    const [text, setText] = useState("")
+
+    const onChangeText = (event)=>setText(event.target.value)
+
     return (
         <div className="inputContainer">
-            <input  className="input" placeholder="holix"/>
+            <input  className="input" placeholder="holix" onChange={onChangeText}/>
         </div>
     )
 }
